22e277ef761c7cc7efc7d94b7929817b0c814165
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-05-10 12:28:00 +0300

    Add all methods

--- Hackathon/HackathonTasks/src/com/tsvetanilievdev/Main.java
+++ Hackathon/HackathonTasks/src/com/tsvetanilievdev/Main.java
@@ -1,19 +1,53 @@
 package com.tsvetanilievdev;
+import java.util.Arrays;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    //Strings
+    public static String abbreviate(String source, int maxLength) {
+        return source.substring(0,maxLength) + "...";
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static boolean contains(String source, char symbol) {
+        int index = source.indexOf(symbol);
+        return index == -1 ? false: true;
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static boolean endsWith(String source, char target) {
+        return source.charAt(source.length() - 1) == target;
+    }
+
+    //Arrays
+    public static int[] addFirst(int[] source, int element) {
+        int[] newArray = new int[source.length + 1];
+        newArray[0] = element;
+
+        for (int i = 0; i < source.length ; i++) {
+            newArray[i + 1] = source[i];
         }
+
+        return newArray;
+
+    }
+
+    public static boolean contains(int[] source, int element) {
+        boolean isContains = false;
+        for (int num: source) {
+            if(num == element) {
+                isContains = true;
+                break;
+            }
+        }
+        return isContains;
+    }
+
+    public static void copyFrom(int[] sourceArray, int sourceStartIndex,
+                                int[] destinationArray, int destStartIndex, int count) {
+        int index = destStartIndex;
+        for (int i = sourceStartIndex; i < sourceStartIndex + 2; i++) {
+            destinationArray[index] = sourceArray[i];
+            index++;
+        }
+
+        System.out.println(Arrays.toString(destinationArray));
     }
 }
