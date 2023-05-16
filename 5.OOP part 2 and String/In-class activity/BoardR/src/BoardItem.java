76d58a9616769c5167245595d05892e4d6c64792
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-05-16 21:03:52 +0300

    Refactor BoardItem

--- 5.OOP part 2 and String/In-class activity/BoardR/src/BoardItem.java
+++ 5.OOP part 2 and String/In-class activity/BoardR/src/BoardItem.java
@@ -2,27 +2,45 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class BoardItem {
-    String title;
-    LocalDate dueDate;
-    Status status;
+    public static final int BOARD_ITEM_MIN_LENGTH = 5;
+    public static final int BOARD_ITEM_MAX_LENGTH = 30;
+    public static final String TITLE_ERROR = String.format("%s length must be between %d and %d characters long!", "Title", BOARD_ITEM_MIN_LENGTH, BOARD_ITEM_MAX_LENGTH);
+    public static final String DATE_ERROR = "The date should never be in the past";
+
+    private String title;
+    private LocalDate dueDate;
+    private Status status;
     Status[] statusArrayList;
     public BoardItem(String title, LocalDate dueDate){
-
-        if(title.length() >= 5 && title.length() <= 30){
-            this.title = title;
-        }else {
-            throw new Error("Title length must be between 5 and 30 characters long!");
-        }
-
-        if(LocalDate.now().isBefore(dueDate)){
-            this.dueDate = dueDate;
-        }else {
-            throw new Error("The date should never be in the past");
-        }
-
+        setTitle(title);
+        setDueDate(dueDate);
         this.status = Status.Open;
 
         this.statusArrayList = Status.values();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setTitle(String title) {
+        if(title.length() < BOARD_ITEM_MIN_LENGTH || title.length() > BOARD_ITEM_MAX_LENGTH) {
+            throw new IllegalArgumentException(TITLE_ERROR);
+        }
+        this.title = title;
+    }
+
+    private void setDueDate(LocalDate dueDate){
+        if(!LocalDate.now().isBefore(dueDate)){
+            throw new IllegalArgumentException(DATE_ERROR);
+        }
+        this.dueDate = dueDate;
     }
 
     public void revertStatus(){
