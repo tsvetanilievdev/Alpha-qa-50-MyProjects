a7f0c8ab00979d81f5fa1bc7cb5f1f592aa0aa65
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-05-21 20:23:45 +0300

    minor changes in Main in BoardR 3

--- 6.OOP Inheritance and Polymorphism/In-class activity/BoardR/src/Main.java
+++ 6.OOP Inheritance and Polymorphism/In-class activity/BoardR/src/Main.java
@@ -2,18 +2,14 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Task task = new Task("Test the application flow", "Pesho", LocalDate.now().plusDays(1));
-        task.advanceStatus();
-        task.advanceStatus();
-        task.setAssignee("Gosho");
-        task.displayHistory();
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+        Issue issue = new Issue("App flow tests?", "", tomorrow);
+        Task task = new Task("Test the application flow", "Pesho", tomorrow);
 
-        Issue issue = new Issue(
-                "App flow tests?",
-                "We need to test the App!",
-                LocalDate.now().plusDays(1));
-        issue.advanceStatus();
-        issue.setDueDate(issue.getDueDate().plusDays(1));
-        issue.displayHistory();
+        Board board = new Board();
+
+        board.addItem(issue);
+        board.addItem(task);
+        System.out.println(board.totalItems()); // 2
     }
 }
