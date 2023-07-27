cb12cf78f7d1d97b6df0aefc53fafb2e5bd56cec
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2023-07-27 20:09:44 +0300

    Add Rest Assurance Practice Project skeleton

--- Web Testing/RestAssurancePractice/src/test/java/restassurancetest/MyFirstTest.java
+++ Web Testing/RestAssurancePractice/src/test/java/restassurancetest/MyFirstTest.java
@@ -1,8 +1,10 @@
 package restassurancetest;
 
+import org.junit.Test;
+
 public class MyFirstTest {
     @Test
     public void testMethod(String[] args) {
-
+        System.out.printf("Success");
     }
 }
